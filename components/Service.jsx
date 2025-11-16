273c705c1248b6f946248463cda530dd6a6e217a
Ino Tech <ezekieldavid150@gmail.com>
2025-11-16 02:10:26 +0100

    Enhance Service component styling with flex layout and updated heading design for improved visual appeal.

--- components/Service.jsx
+++ components/Service.jsx
@@ -3,8 +3,15 @@ import { IoCode } from "react-icons/io5";
 
 export default function Service() {
     return (
-        <section id="services">
-            <h2>My Services</h2>
+        <section id="services" className="flex flex-col justify-center items-center text-center px-4 py-6">
+            <div>
+                <h2 className="text-4xl text-blue-500 mb-5" style={{ fontFamily: "cursive" }}>My Services</h2>
+                
+            </div>
+            
+            <div>
+                
+            </div>
         </section>
     )
 }
