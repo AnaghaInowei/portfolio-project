1723c250f36f7adc988c3055183ace0698357639
Ino Tech <ezekieldavid150@gmail.com>
2025-11-17 13:12:19 +0100

    Refactor Footer component to enhance styling and layout. Update headings with bold and cursive fonts, and add hover effects to social media links and navigation items for improved user experience.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,11 +10,11 @@ import { SiGmail } from "react-icons/si";
 
 export default function Footer() {
   return (
-    <section className="bg-gray-900 text-gray-100">
+    <section className="bg-gray-900 text-gray-100 ">
       <div className="grid grid-cols-1 md:grid-cols-3  justify-between px-3 py-6  gap-3">
         {/* Quick Links */}
         <div>
-          <h2 className="flex items-center gap-2">
+          <h2 className="flex items-center gap-2font-bold text-gray-50 text-2xl mb-4" style={{ fontFamily: "cursive" }}>
             <FaCode className="text-blue-500 hover:animate-spin cursor-pointer" />
             Inowei
           </h2>
@@ -23,27 +23,27 @@ export default function Footer() {
             technologies.
           </p>
           <div className="flex flex-col gap-3 text-2xl">
-            <FaLinkedin />
-            <FaGithub />
-            <FaWhatsapp />
-            <FaXTwitter />
+            <FaLinkedin className="hover:text-blue-400" />
+            <FaGithub className="hover:text-blue-400" />
+            <FaWhatsapp className="hover:text-blue-400" />
+            <FaXTwitter className="hover:text-blue-400" />
           </div>
         </div>
 
         {/* Quick Links */}
         <div>
-          <h2>Quick Links</h2>
+          <h2 className="font-bold text-blue-500 text-2xl mb-4 " style={{ fontFamily: "cursive" }}>Quick Links</h2>
           <div className="flex flex-col justify-start gap-3">
-            <a href="#hero">Home</a>
-            <a href="#about">About</a>
-            <a href="#projects">Projects</a>
-            <a href="#services">Services</a>
-            <a href="#contact">Contact</a>
+            <a className="hover:text-blue-400" href="#hero">Home</a>
+            <a className="hover:text-blue-400" href="#about">About</a>
+            <a className="hover:text-blue-400" href="#projects">Projects</a>
+            <a className="hover:text-blue-400" href="#services">Services</a>
+            <a className="hover:text-blue-400" href="#contact">Contact</a>
           </div>
         </div>
 
         <div>
-          <h2>Contact Info</h2>
+          <h2 className="font-bold text-blue-500 text-2xl mb-4 " style={{ fontFamily: "cursive" }}>Contact Info</h2>
           <div className="flex flex-col gap-2 justify-start">
             <p className="flex justify-start items-center hover:text-blue-500 transition-all duration-300 text-white gap-3">
               <FaLocationDot /> Port-Harcourt, Nigeria
